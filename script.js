f67ea4fe6f91c48cc2910e24185c2ac042dd88dd
panagiotis_papantonis <103947010+PanagiotisPapantonis@users.noreply.github.com>
2023-03-15 21:54:11 +0200

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,10 @@ let navbar = document.querySelector('.navbar');
 menuIcon.onclick = () => {
     menuIcon.classList.toggle("bx-x");
     navbar.classList.toggle('active');
-}
-
+    
     menuIcon.classList.remove("bx-x");
     navbar.classList.remove('active');
+
+}
+
+   
