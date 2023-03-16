1c2d9be2953879c0f545d57647419dcf3918e7f7
IEEE Lab PC 3 <103947010+PanagiotisPapantonis@users.noreply.github.com>
2023-03-16 12:49:54 +0200

    color-fixing

--- script.js
+++ script.js
@@ -11,3 +11,12 @@ menuIcon.onclick = () => {
 menuIcon.classList.remove("bx-x");
 navbar.classList.remove('active');
    
+
+ScrollReveal({
+     reset: true,
+     distance: '80px',
+     duration: 2000,
+     delay: 250 
+    });
+
+    ScrollReveal().reveal('.home-content, .heading', { origin:"top" });
